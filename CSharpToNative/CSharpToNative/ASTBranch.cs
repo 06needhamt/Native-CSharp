b87852b61fdb371690ea5b1692b0cf05ea5fd662
06needhamt <06needhamt@gmail.com>
2015-01-20 09:53:38 +0000

    fixed astbranch

--- CSharpToNative/CSharpToNative/ASTBranch.cs
+++ CSharpToNative/CSharpToNative/ASTBranch.cs
@@ -140,7 +140,7 @@ namespace CSharpToNative
                 }
                 // assign the tempory values to the actual tree
                 this.type = etypeval;
-                this.operation = eopval;
+                this.set = eopval;
                 this.protectionlevel = eprotval;
                 Console.WriteLine(Convert.ToString(this.type));
                 Console.WriteLine(Convert.ToString(this.operation));
@@ -162,93 +162,14 @@ namespace CSharpToNative
                 tree.ASTbranches.Add(this); // add the branch to the tree
             }
 
-            
-
-            /* EnumAccessModifiers eprotval = EnumAccessModifiers.NO_MODIFIER;
-            EnumTypes etypeval = EnumTypes.NO_TYPE;
-            EnumOperator eopval = EnumOperator.NO_OPERATOR;
-
-            if (tokens.Contains(Convert.ToString(EnumTypes.STATIC)) && tokens.Contains(Convert.ToString(EnumKeywords.VOID)))
-            {
-                //int protval = Convert.ToInt32(tokens[0]);
-                eprotval = (EnumAccessModifiers)Enum.Parse(typeof(EnumAccessModifiers), tokens[0].ToUpper());
-                //int typeval = Convert.ToInt32(tokens[1]);
-                etypeval = (EnumTypes)Enum.Parse(typeof(EnumTypes), tokens[1].ToUpper() + tokens[2].ToUpper());
-                //int opval = Convert.ToInt32(tokens[2]);
-                eopval = EnumOperator.NO_OPERATOR;
-                this.name = tokens[4];
-                this.Value = null;
-            }
-            else if (tokens.Contains(Convert.ToString(EnumTypes.STATIC)))
-            {
-                //int protval = Convert.ToInt32(tokens[0]);
-                eprotval = (EnumAccessModifiers)Enum.Parse(typeof(EnumAccessModifiers), tokens[0].ToUpper());
-                //int typeval = Convert.ToInt32(tokens[1]);
-                etypeval = (EnumTypes)Enum.Parse(typeof(EnumTypes), tokens[1].ToUpper() + "_" + tokens[2].ToUpper());
-                //int opval = Convert.ToInt32(tokens[2]);
-                eopval = EnumOperator.NO_OPERATOR; //(EnumOperator)Enum.Parse(typeof(EnumOperator), tokens[3].ToUpper());
-                this.name = tokens[4];
-                if (tokens.Length > 5)
-                {
-                    this.Value = tokens[5];
-                }
-                else
-                {
-                    this.Value = null;
-                }
-            }
-            else
-            {
-                if ((int)eprotval > 2)
-                {
-                    this.protectionlevel = EnumAccessModifiers.NO_MODIFIER;
-                }
-                else
-                {
-                    this.protectionlevel = (EnumAccessModifiers)Enum.Parse(typeof(EnumAccessModifiers), tokens[0].ToUpper());
-                }
-
-                if ((int)etypeval > 12)
-                {
-                    this.type = EnumTypes.NO_TYPE;
-                }
-                else if (etypeval == EnumTypes.STATIC_VOID)
-                {
-                    this.type = (EnumTypes)Enum.Parse(typeof(EnumTypes), tokens[1].ToUpper() + "_" + tokens[2].ToUpper());
-                }
-                else if(etypeval == EnumTypes.STATIC || tokens[1].ToUpper().Equals("STATIC"))
-                {
-                    if (tokens[2].ToUpper().Equals("CONST"))
-                    {
-                        this.type = (EnumTypes)Enum.Parse(typeof(EnumTypes), tokens[3].ToUpper());
-                    }
-                    else
-                    {
-                        this.type = (EnumTypes)Enum.Parse(typeof(EnumTypes), tokens[2].ToUpper());
-                    }
-                }
-                else if (etypeval == EnumTypes.CONST || tokens[1].ToUpper().Equals("CONST"))
-                {
-                    //if (tokens[2].ToUpper().Equals("CONST"))
-                    //{
-                    //    this.type = (EnumTypes)Enum.Parse(typeof(EnumTypes), tokens[3].ToUpper());
-                    //}
-
-                    this.type = (EnumTypes)Enum.Parse(typeof(EnumTypes), tokens[2].ToUpper());
-                }
-                else
-                {
-                    this.type = (EnumTypes)Enum.Parse(typeof(EnumTypes), tokens[1].ToUpper());
-                }
-
-                if ((int)eopval >= 36 || (int)eopval < 0)
-                {
-                    this.operation = EnumOperator.NO_OPERATOR;
-                }
-                else
-                {
-                    this.operation = (EnumOperator)Enum.Parse(typeof(EnumOperator), tokens[2].ToUpper());
-                }*/
+        }
+        public bool GetIsRoot()
+        {
+            return this.isroot;
+        }
+        public void SetIsRoot(bool b)
+        {
+            this.isroot = b;
         }
     }
 }
