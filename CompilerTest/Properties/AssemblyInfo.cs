8bf89dbb37b42f91566bb0b7a1a8bd796b60a435
06needhamt <06needhamt@gmail.com>
2015-04-12 13:41:18 +0100

    added command line arguments

--- CompilerTest/Properties/AssemblyInfo.cs
+++ CompilerTest/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.5580.17885")]
+[assembly: AssemblyVersion("1.2.5580.22723")]
 //[assembly: AssemblyFileVersion("1.1.0.56547")]
