f1b8690c9cc4fd2b4ffc0b01fd47b638d25113cd
06needhamt <06needhamt@gmail.com>
2015-06-01 12:18:48 +0100

    symbol tables are now saved

---
+++ CSharpToNative/SymbolTable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Native.CSharp.Compiler
+{
+    internal class SymbolTable<T>
+    {
+        private LinkedList<T> symbols;
+
+        public LinkedList<T> Symbols
+        {
+            get { return symbols; }
+            set { symbols = value; }
+        }
+        private readonly Type symbolType;
+
+        public Type SymbolType
+        {
+            get { return symbolType; }
+        } 
+ 
+
+        public SymbolTable(LinkedList<T> symbols)
+        {
+            this.symbols = symbols;
+            this.symbolType = typeof(T);
+        }
+
+        public T findSymbol(Symbol<T> symbolToFind)
+        {
+            return this.Symbols.Find(symbolToFind.Value).Value;
+        }
+    }
+}
